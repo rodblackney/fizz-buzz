ffb82116898b2dbcc6872fc6fbc630b2bc6d9dad
Rod Blackney <rblackney@gmail.com>
2015-05-24 20:03:39 +1000

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var count = 1;
+while (count <= 100) {
+  // divisible by both 3 and 5 = fizz buzz
+  if (count % 3 === 0 &&
+    count % 5 === 0) {
+    console.log('fizzbuzz');
+    $('#numbers').append($('<li>fizzbuzz</li>').css('color', 'orange'));
+  }
+
+  // divisible by 3 = fizz
+  else if (count % 3 === 0) {
+    console.log('fizz');
+    $('#numbers').append($('<li>fizz</li>').css('color', 'darkorange'));
+  }
+  // divisible by 5 = buzz    
+  else if (count % 5 === 0) {
+    console.log('buzz');
+    $('#numbers').append($('<li>buzz</li>').css('color', 'maroon'));
+  }
+  // numbers 
+  else {
+    console.log(count);
+    $('#numbers').append('<li>' + count + '</li>');
+  }
+  count = count + 1;
+}
